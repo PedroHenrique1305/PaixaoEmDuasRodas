92b69306c84f9dcda3518c3cc3fbca815414813e
PedroHenrique1305 <pedro.cardoso@sptech.school>
2024-05-31 00:31:07 -0300

    finalização do grafico Usuario

--- web-data-viz/src/models/medidaModel.js
+++ web-data-viz/src/models/medidaModel.js
@@ -2,7 +2,8 @@ var database = require("../database/config");
 
 function buscarUltimasMedidas(idUsuario) {
     console.log("ACESSEI O MEDIDA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n buscarUltimasMedidas(): ")
-    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 1)) AS CHAR), 2) AS percentagem_respostaUsuarios_certas1 FROM resposta WHERE correta = 'sim' AND fkQuestao = 1 and fkUsuario = ${idUsuario};`;
+    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 1 and fkUsuario = ${idUsuario})) AS CHAR), 2) AS percentagem_respostasUsuario_certas1
+    FROM resposta WHERE correta = 'sim' AND fkQuestao = 1 and fkUsuario = ${idUsuario};`;
     
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -10,7 +11,8 @@ function buscarUltimasMedidas(idUsuario) {
 
 function buscarUltimasMedidas2(idUsuario) {
     console.log("ACESSEI O MEDIDA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function buscarUltimasMedidas2(): ")
-    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 2)) AS CHAR), 2) AS percentagem_respostaUsuarios_certas2 FROM resposta WHERE correta = 'sim' AND fkQuestao = 2 and fkUsuario = ${idUsuario};`;
+    
+    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 2 and fkUsuario = ${idUsuario})) AS CHAR), 2) AS percentagem_respostaUsuarios_certas2 FROM resposta WHERE correta = 'sim' AND fkQuestao = 2 and fkUsuario = ${idUsuario};`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
@@ -18,7 +20,7 @@ function buscarUltimasMedidas2(idUsuario) {
 
 function buscarUltimasMedidas3(idUsuario) {
     console.log("ACESSEI O MEDIDA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function buscarUltimasMedidas2(): ")
-    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 1)) AS CHAR), 2) AS percentagem_respostasUsuario_erradas1 FROM resposta WHERE correta = 'nao' AND fkQuestao = 1 and fkUsuario = ${idUsuario};`;
+    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 1 and fkUsuario = ${idUsuario})) AS CHAR), 2) AS percentagem_respostasUsuario_erradas1 FROM resposta WHERE correta = 'nao' AND fkQuestao = 1 and fkUsuario = ${idUsuario};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -27,7 +29,7 @@ function buscarUltimasMedidas3(idUsuario) {
 
 function buscarUltimasMedidas4(idUsuario) {
     console.log("ACESSEI O MEDIDA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function buscarUltimasMedidas2(): ")
-    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 2)) AS CHAR), 2) AS percentagem_respostasUsuario_erradas2 FROM resposta WHERE correta = 'nao' AND fkQuestao = 2 and fkUsuario = ${idUsuario};`;
+    var instrucaoSql = `SELECT LEFT(CAST((COUNT(*) * 100 / (SELECT COUNT(*) FROM resposta WHERE fkQuestao = 2 and fkUsuario = ${idUsuario})) AS CHAR), 2) AS percentagem_respostasUsuario_erradas2 FROM resposta WHERE correta = 'nao' AND fkQuestao = 2 and fkUsuario = ${idUsuario};`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
